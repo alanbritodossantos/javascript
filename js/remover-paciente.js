6187dbd983f72e6be2288fa902d72f228c208942
Alan <alanbritodossantos2015@gmail.com>
2022-03-17 03:05:29 -0300

    Delegando eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,13 +1,20 @@
-var pacientes = document.querySelectorAll(".paciente");//pega os paciente que estão na pagina
+var pacientes = document.querySelectorAll(".paciente");//pega os paciente que estão na paginag
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick",function(){//ao ouvir o duplo clique execute a função anonima
-        console.log("fui clicado com dublo click!");
-        alert("Paciente excluído");
-        this.remove();
-    });
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick",function(){//ao ouvir o duplo clique execute a função anonima
+//         console.log("fui clicado com dublo click!");
+//         alert("Paciente excluído");
+//         this.remove();
+//     });
+// });
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick",function(event){
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; //remova o pai desse event
+    paiDoAlvo.remove();
 });
-
 
 
 
